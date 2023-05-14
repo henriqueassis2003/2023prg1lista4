5195e577bd6c3f08641e5f19d54f2960f3b7ec7c
henriqueassis2003 <128740531+henriqueassis2003@users.noreply.github.com>
2023-05-14 20:37:49 -0300

    Update Questao 5.py

--- Questao 5.py
+++ Questao 5.py
@@ -1,25 +1,11 @@
-def search_rotated(nums, target):
-   
-    n = len(nums)
-    if n == 0:
-        return -1
+def hanoi(n, origem, destino, auxiliar):
+    if n == 1:
+        print(f"Mover disco 1 da pilha {origem} para a pilha {destino}")
+    else:
+        hanoi(n-1, origem, auxiliar, destino)
+        print(f"Mover disco {n} da pilha {origem} para a pilha {destino}")
+        hanoi(n-1, auxiliar, destino, origem)
 
-    left, right = 0, n-1
-    while left < right:
-        mid = (left + right) // 2
-        if nums[mid] > nums[right]:
-            left = mid + 1
-        else:
-            right = mid
-    pivot = left
-    left, right = 0, n-1
-    while left <= right:
-        mid = (left + right) // 2
-        mid_pivot = (mid + pivot) % n
-        if nums[mid_pivot] == target:
-            return mid_pivot
-        elif nums[mid_pivot] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
+# Exemplo de uso
+hanoi(3, 'Esquerda', 'Direita', 'Auxiliar')
+
